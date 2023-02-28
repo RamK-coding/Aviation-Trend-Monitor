7b90d2bf9371b560b217813961771e94bf2edd42
SystemsThinker <116301123+RamK-coding@users.noreply.github.com>
2023-02-28 14:18:07 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -5,12 +5,3 @@ st.set_page_config(layout="wide", initial_sidebar_state='expanded')
 st.image("BHL.png")
 st.title("Welcome to the Aviation Trend Monitor!")
 st.caption(":blue[Created by Ram Kamath :sunglasses:]")
-
-#****Creating a TOML file****
-#data = { "key": ""
-#       }
-
-#toml_string = toml.dumps(data)  # Output to a string
-#output_file_name = "output.toml"
-#with open(output_file_name, "w") as toml_file:
-#    toml.dump(data, toml_file)

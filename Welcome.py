ac0fa41e3bceb269911afd922cd8621b2b8b984e
SystemsThinker <116301123+RamK-coding@users.noreply.github.com>
2023-02-28 14:25:23 +0100

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -2,6 +2,6 @@ import streamlit as st
 import toml
 
 st.set_page_config(layout="wide", initial_sidebar_state='expanded')
-st.image("BHL.png")
+st.image("BHL.PNG")
 st.title("Welcome to the Aviation Trend Monitor!")
 st.caption(":blue[Created by Ram Kamath :sunglasses:]")

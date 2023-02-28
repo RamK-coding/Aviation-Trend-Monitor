9354809bc303fce513b1554715e9b8c8bade7716
Ram Kamath <Ram.Kamath@bauhaus-luftfahrt.corp>
2023-02-28 15:58:52 +0100

    Small changes

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(layout="wide", initial_sidebar_state='expanded')
-st.image("BHL.png")
+st.image("BHL.PNG")
 st.title("Welcome to the Aviation Trend Monitor!")
 st.caption(":blue[Created by Ram Kamath :sunglasses:]")
 
